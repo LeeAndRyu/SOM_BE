384c42f464782e34dba3f5696e27eb55911e2007
HyunSik Ryu <hsryu123@naver.com>
2024-01-13 22:56:11 +0900

    initial commit

---
+++ src/main/java/com/blog/som/SomApplication.java
@@ -0,0 +1,13 @@
+package com.blog.som;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SomApplication.class, args);
+  }
+
+}
