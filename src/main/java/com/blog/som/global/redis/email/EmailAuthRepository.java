e8acf86d1814f19a6f7632e5d925e6919db6fe94
HyunSik Ryu <hsryu123@naver.com>
2024-01-17 16:12:30 +0900

    Feat : Redis 세팅, EmailAuth를 위한 RedisRepository 구현

---
+++ src/main/java/com/blog/som/global/redis/email/EmailAuthRepository.java
@@ -0,0 +1,11 @@
+package com.blog.som.global.redis.email;
+
+public interface EmailAuthRepository {
+
+  void saveEmailAuthUuid(String uuid, Long id);
+
+  Long getEmailAuthMemberId(String uuid);
+
+  void deleteEmailAuthUuid(String uuid);
+
+}
