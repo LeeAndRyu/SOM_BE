8439e9a6712356693208aa5e7ee3e5337f5ea663
HyunSik Ryu <hsryu123@naver.com>
2024-01-19 05:26:04 +0900

    Docs : Swagger API 컨트롤러 명 수정

--- src/main/java/com/blog/som/domain/member/controller/AuthController.java
+++ src/main/java/com/blog/som/domain/member/controller/AuthController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
-@Api(tags = "인증(Login, Logout, Token 등)")
+@Api(tags = "인증 관련 (login, logout)")
 @RequiredArgsConstructor
 @RestController
 public class AuthController {
