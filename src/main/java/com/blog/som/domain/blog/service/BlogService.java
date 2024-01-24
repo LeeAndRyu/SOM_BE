855c5246fc6259f8af9e3dffdaa4fe5e22c027cc
HyunSik Ryu <hsryu123@naver.com>
2024-01-24 21:17:39 +0900

    Docs : 서비스 메서드에 JavaDoc 주석 작성

--- src/main/java/com/blog/som/domain/blog/service/BlogService.java
+++ src/main/java/com/blog/som/domain/blog/service/BlogService.java
@@ -5,8 +5,28 @@ import com.blog.som.domain.blog.dto.BlogMemberDto;
 import com.blog.som.domain.blog.dto.BlogPostList;
 
 public interface BlogService {
+
+  /**
+   * 블로그 회원 정보 조회
+   */
   BlogMemberDto getBlogMember(String accountName);
+
+  /**
+   * 정렬 방식에 따른 postList 조회
+   * - sort=latest(default) : 최신 순
+   * - sort=hot : 조회수 순
+   */
   BlogPostList getBlogPostListBySortType(String accountName, String sort, int page);
+
+  /**
+   * tagName 태그를 가진 postList 조회
+   * - 정렬 : 최신 순
+   */
   BlogPostList getBlogPostListByTag(String accountName, String tagName, int page);
+
+  /**
+   * title 또는 introduction에 "query"를 포함한 postList 조회
+   * - 정렬 : 최신 순
+   */
   BlogPostList getBlogPostListByQuery(String accountName, String query, int page);
 }
--- src/main/java/com/blog/som/domain/member/service/MemberService.java
+++ src/main/java/com/blog/som/domain/member/service/MemberService.java
@@ -1,6 +1,5 @@
 package com.blog.som.domain.member.service;
 
-import com.blog.som.domain.member.dto.EmailAuthResult;
 import com.blog.som.domain.member.dto.MemberDto;
 import com.blog.som.domain.member.dto.MemberEditRequest;
 import com.blog.som.domain.member.dto.MemberPasswordEdit;
@@ -9,15 +8,33 @@ import org.springframework.web.multipart.MultipartFile;
 
 public interface MemberService {
 
+  /**
+   * 회원 가입 이전에 이메일 중복 확인 결과 반환, 중복이 아닐 시 회원 가입 메일 발송
+   */
   MemberRegister.EmailDuplicateResponse emailDuplicateCheckAndStartRegister(String email);
 
+  /**
+   * 회원 가입
+   */
   MemberDto registerMember(MemberRegister.Request request, String code);
 
+  /**
+   * 회원 정보 수정
+   */
   MemberDto editMemberInfo(Long memberId, MemberEditRequest request);
 
+  /**
+   * 비밀번호 수정
+   */
   MemberPasswordEdit.Response editMemberPassword(Long memberId, MemberPasswordEdit.Request request);
 
+  /**
+   * 프로필 사진 등록 또는 수정
+   */
   MemberDto updateProfileImage(Long memberId, MultipartFile profileImage);
 
+  /**
+   * 프로필 사진 삭제
+   */
   MemberDto deleteProfileImage(Long memberId);
 }
